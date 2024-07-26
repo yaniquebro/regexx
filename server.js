c7656e3440bdb0567463d9a5d6f380ea64221ec7
Yanique Brooks <yaniquebro@gmail.com>
2024-07-25 20:27:17 -0400

    connection js, db, server.js

---
+++ server.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const path = require("path");
+const express = require('express');
+const routes = require('./routes');
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({
+  helpers,
+});
+
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: process.env.DB_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+    checkExpirationInterval: 1000 * 60 * 10, // checks every 10m
+    expiration: 1000 * 60 * 30, // expires after 30 minutes
+  }),
+};
+
+// import sequelize connection
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(session(sess));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: true })
+.then(() => {
+  app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
+})
