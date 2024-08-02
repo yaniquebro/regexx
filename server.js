109a0e164e8bfabbfc01ebc4770e0f422592bf49
Yanique Brooks <yaniquebro@gmail.com>
2024-08-02 16:05:50 -0400

    updates

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const path = require("path");
 const express = require('express');
-const routes = require('./routes');
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
