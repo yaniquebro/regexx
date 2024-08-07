164eebb176390e286168e4787f4b17c461471ab6
Yanique Brooks <yaniquebro@gmail.com>
2024-08-07 08:48:45 -0400

    .env updates

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: process.env.DB_SECRET,
+  secret: process.env.SESSION_SECRET || SESSION_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
