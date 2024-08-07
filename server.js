2ad1f8478be8f724a6cf3ef74f3a1bf0e141bfd6
Yanique Brooks <yaniquebro@gmail.com>
2024-08-07 09:49:40 -0400

    idk anymore

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-require("dotenv").config();
-const path = require("path");
+require('dotenv').config();
+const path = require('path');
 const express = require('express');
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
-const helpers = require("./utils/helpers");
-const exphbs = require("express-handlebars");
-const hbs = exphbs.create({
-  helpers,
-});
+const session = require('express-session');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
 
-const session = require("express-session");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const hbs = exphbs.create({ helpers });
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const sess = {
   secret: process.env.SESSION_SECRET || SESSION_SECRET,
